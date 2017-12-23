59e9891073716972cee3909dd50459006ba06f91
ibshaikit <ibrahim.shaik.786@gmail.com>
2017-12-23 07:57:20 +0530

    First commit

---
+++ Programs/src/com/ib/programs/SampleOne.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ib.programs;
+
+/**
+ * @author ibrahim
+ *
+ */
+public class SampleOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Java Program");
+	}
+
+}
